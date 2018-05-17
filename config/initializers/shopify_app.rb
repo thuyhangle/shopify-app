e44fe9abb704451f4c98c92c1545a6bc729790a3
Hangle <hang@agencyleroy.com>
2018-05-17 16:55:43 +0300

    Generetors

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "77cecbf22b02bcc3d71a82ed8b2d481f"
   config.secret = "4b832568fd9c45f8251349fa9ede0862"
+  config.redirect_uri = "http://localhost:3000/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
