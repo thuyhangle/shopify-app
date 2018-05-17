9c70c8f3be9f701adcb32eb08cf14685b78fea50
Hangle <hang@agencyleroy.com>
2018-05-17 16:37:41 +0300

    Add key

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,9 @@
+ShopifyApp.configure do |config|
+  config.application_name = "My Shopify App"
+  config.api_key = "77cecbf22b02bcc3d71a82ed8b2d481f"
+  config.secret = "4b832568fd9c45f8251349fa9ede0862"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+  config.after_authenticate_job = false
+  config.session_repository = Shop
+end
